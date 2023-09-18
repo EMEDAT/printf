c229574c81c63ba456f6e12169d8ca74da1ed7f4
cod-emminex <emminexy@yahoo.com>
2023-09-17 17:46:25 -0700

    Precision was checking wrong

--- main.h
+++ main.h
@@ -70,10 +70,6 @@ int print_hexa_upper(va_list types, char buffer[],
 int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
-
-/* Function to handle precision */
-int handle_precision(va_list args, const char *modifier, char *index)
-
 /* Function to print non printable characters */
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
