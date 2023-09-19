3d2b835c69cddb7e0eb4649541ee07031590935d
arafahabiodun <arafahabiodun@gmail.com>
2023-09-18 17:42:48 -0700

    Debugging

--- function.c
+++ function.c
@@ -1,24 +1,4 @@
 #include "main.h"
-
-/************************* PRINT CHAR *************************/
-
-/**
- * print_char - Prints a char
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: Width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
- */
-int print_char(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	char c = va_arg(types, int);
-
-	return (handle_write_char(c, buffer, flags, width, precision, size));
-}
 /************************* PRINT A STRING *************************/
 /**
  * print_string - Prints a string
@@ -74,6 +54,29 @@ int print_string(va_list types, char buffer[],
 
 	return (write(1, str, length));
 }
+
+
+/********************* PRINT CHAR *********************/
+
+/**
+ * print_char - Prints a char
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char c = va_arg(types, int);
+
+	return (handle_write_char(c, buffer, flags, width, precision, size));
+}
+
 /************************* PRINT PERCENT SIGN *************************/
 /**
  * print_percent - Prints a percent sign
